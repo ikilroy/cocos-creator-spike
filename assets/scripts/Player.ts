fcede47ad5d37e5188fce6fc015e01f24424f0f6
Izabela Kilroy <izabela.kilroy@gmail.com>
2019-09-24 12:01:36 +0800

    Game scene and Player.ts script

---
+++ assets/scripts/Player.ts
@@ -0,0 +1,34 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Player extends cc.Component {
+
+    // In pixels.
+    @property
+    jumpHeight: number = 0;
+
+    // In seconds.
+    @property
+    jumpDuration: number = 0;
+
+    // In pixels per second.
+    @property
+    maxSpeed: number = 0;
+
+    // In pixels per second.
+    @property
+    accel: number = 0;
+
+    onLoad() {
+        let jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        let jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        let jumpAction = cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+        this.node.runAction(jumpAction);
+    }
+
+    start() {
+
+    }
+
+    // update (dt) {}
+}
