096322e441cc25329c022f9321ed56372949fc84
Izabela Kilroy <izabela.kilroy@gmail.com>
2019-09-24 16:32:44 +0800

    Player movement continued

--- assets/scripts/Player.ts
+++ assets/scripts/Player.ts
@@ -22,16 +22,74 @@ export default class Player extends cc.Component {
     @property
     accel: number = 0;
 
-    onLoad() {
-        let jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
-        let jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
-        let jumpAction = cc.repeatForever(cc.sequence(jumpUp, jumpDown));
-        this.node.runAction(jumpAction);
+    // Flag to move right or left.
+    goLeft: boolean = false;
+
+    // Flag to move right or left.
+    goRight: boolean = false;
+
+    // Number of pixels from the centre of the scene to its edge.
+    offScreen = 0
+
+    onLoad(): void {
+        this.offScreen = this.node.parent.width / 2 + this.node.width / 2;
+        this.node.runAction(this.constructJumpAction());
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
     }
 
     start() {
 
     }
 
-    // update (dt) {}
+    update(dt: number): void {
+        if (this.goRight) {
+            this.xSpeed += this.accel * dt;
+        } else if (this.goLeft) {
+            this.xSpeed -= this.accel * dt;
+        }
+        if (Math.abs(this.xSpeed) > this.maxSpeed) {
+            this.xSpeed = this.maxSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+        this.node.x += this.xSpeed * dt;
+        if (this.node.position.x < -this.offScreen) {
+            this.node.x = this.offScreen;
+        } else if (this.node.position.x > this.offScreen) {
+            this.node.x = -this.offScreen;
+        }
+    }
+
+    constructJumpAction(): cc.ActionInterval {
+        let jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCircleActionOut());
+        let jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCircleActionIn());
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    }
+
+    onKeyDown(event: KeyboardEvent): void {
+        switch (event.keyCode) {
+            case cc.macro.KEY.left:
+                this.goLeft = true;
+                break;
+            case cc.macro.KEY.right:
+                this.goRight = true;
+                break;
+        }
+    }
+
+    onKeyUp(event: KeyboardEvent): void {
+        switch (event.keyCode) {
+            case cc.macro.KEY.left:
+                this.goLeft = false;
+                break;
+            case cc.macro.KEY.right:
+                this.goRight = false;
+                break;
+        }
+    }
+
+    onDestroy(): boolean {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        return true;
+    }
 }
