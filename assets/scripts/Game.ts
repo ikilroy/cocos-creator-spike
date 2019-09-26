3f0b7a63bbfb280450aa7816e140f0db198910c2
Izabela Kilroy <izabela.kilroy@gmail.com>
2019-09-26 12:04:07 +0800

    Spawning star in random position

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -19,6 +19,27 @@ export default class Game extends cc.Component {
     @property
     minStarDuration: number = 0;
 
+    groundY: number = 0;
+
+    onLoad(): void {
+        this.groundY = this.ground.y + this.ground.height / 2;
+        this.spawnNewStar();
+    }
+
     start(): void {
+    }
+
+    spawnNewStar(): void {
+        var newStar = cc.instantiate(this.star);
+        this.node.addChild(newStar);
+        newStar.setPosition(this.starPosition());
+    }
+
+    starPosition(): cc.Vec2 {
+        var randX = 0;
+        var randY = this.groundY + Math.random() * this.player.getComponent('Player').jumpHeight + 50;
+        var maxX = this.node.width / 2;
+        randX = (Math.random() - 0.5) * 2 * maxX;
+        return cc.v2(randX, randY);
     }
 }
